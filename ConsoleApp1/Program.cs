816eca0ea789726a70f705cb72e97e62350184f5
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 18:22:19 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main()
+	{
+		Console.WriteLine("num1");
+		int num1 = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("num2");
+		int num2 = Convert.ToInt32(Console.ReadLine());
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		Console.WriteLine("num1 & num2 is " + num1 +" & " +num2);
+	}
+}
